a855070b9fae64c88fce1d8da773d41b1ea7a9df
jetstream1128 <bogdanalexeev2007@live.com>
2023-05-01 20:17:58 -0600

    1

---
+++ starter/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+//https://restcountries.com/v3.1/all
